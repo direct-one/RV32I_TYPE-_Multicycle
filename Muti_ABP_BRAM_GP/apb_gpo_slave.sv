8e5dd925db87021d18f6a552c5fb8a773d736ddf
direct-one <kimsung226@gmail.com>
2026-03-26 19:16:04 +0900

    Muti_ABP_BRAM_GP
    GPO is complete, GPI is no complete

---
+++ Muti_ABP_BRAM_GP/apb_gpo_slave.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2026/03/26 11:28:56
+// Design Name: 
+// Module Name: apb_gpo_slave
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module apb_gpo_slave(
+
+    );
+endmodule
